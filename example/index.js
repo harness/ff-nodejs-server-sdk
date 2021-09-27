52e99d6e3b42983d738759a4759452e07c8706ac
Enver Bisevac <enver.bisevac@harness.io>
2021-09-27 22:44:10 +0200

    examples updated

--- example/index.js
+++ example/index.js
@@ -1,4 +1,5 @@
-import { Client } from 'ff-nodejs-server-sdk';
+const pkg = require('ff-nodejs-server-sdk');
+const { Client } = pkg;
 
 const client = new Client('1c100d25-4c3f-487b-b198-3b3d01df5794');
 
