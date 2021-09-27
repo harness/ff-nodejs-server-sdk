6bbe872190ba73b4e71cdb06cc7871de002f6e66
Enver Bisevac <enver.bisevac@harness.io>
2021-09-27 09:37:23 +0200

    add singleton for CfClient

--- src/index.ts
+++ src/index.ts
@@ -4,37 +4,37 @@ import { Options, Target } from "./types";
 export { Client, Options, Target };
 export default {
   instance: undefined,
-  // init: function(sdkKey: string, options: Options): void {
-  //   if (!this.instance) {
-  //     this.instance = new Client(sdkKey, options);
-  //   }
-  // },
-  // boolVariation: function(
-  //   identifier: string,
-  //   target: Target,
-  //   defaultValue = false,
-  // ): boolean {
-  //   return this.instance.boolVariation(identifier, target, defaultValue);
-  // },
-  // stringVariation: function(
-  //   identifier: string,
-  //   target: Target,
-  //   defaultValue = '',
-  // ): string {
-  //   return this.stringVariation(identifier, target, defaultValue);
-  // },
-  // numberVariation: function(
-  //   identifier: string,
-  //   target: Target,
-  //   defaultValue = 0,
-  // ): number {
-  //   return this.instance.numberVariation(identifier, target, defaultValue);
-  // },
-  // jsonVariation: function(
-  //   identifier: string,
-  //   target: Target,
-  //   defaultValue = '',
-  // ): Record<string, unknown> {
-  //   return this.instance.jsonVariation(identifier, target, defaultValue);
-  // },
+  init: function(sdkKey: string, options: Options): void {
+    if (!this.instance) {
+      this.instance = new Client(sdkKey, options);
+    }
+  },
+  boolVariation: function(
+    identifier: string,
+    target: Target,
+    defaultValue = false,
+  ): boolean {
+    return this.instance.boolVariation(identifier, target, defaultValue);
+  },
+  stringVariation: function(
+    identifier: string,
+    target: Target,
+    defaultValue = '',
+  ): string {
+    return this.stringVariation(identifier, target, defaultValue);
+  },
+  numberVariation: function(
+    identifier: string,
+    target: Target,
+    defaultValue = 0,
+  ): number {
+    return this.instance.numberVariation(identifier, target, defaultValue);
+  },
+  jsonVariation: function(
+    identifier: string,
+    target: Target,
+    defaultValue = '',
+  ): Record<string, unknown> {
+    return this.instance.jsonVariation(identifier, target, defaultValue);
+  },
 };
