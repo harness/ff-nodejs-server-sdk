a90cfbab18e9ad1d217f75d9a7bd00fd59be4484
Enver Bisevac <enver.bisevac@harness.io>
2021-10-01 10:18:27 +0200

    Logger and KeyValueStore are exported interfaces

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,38 @@
-import Client from "./client";
-import { Options, Target } from "./types";
+import Client from './client';
+import { Options, Target } from './types';
+import { Logger } from './log';
+import { KeyValueStore } from './types';
 
-export { Client, Options, Target };
+export { Client, Options, Target, KeyValueStore, Logger };
 export default {
   instance: undefined,
-  init: function(sdkKey: string, options: Options): void {
+  init: function (sdkKey: string, options: Options): void {
     if (!this.instance) {
       this.instance = new Client(sdkKey, options);
     }
   },
-  boolVariation: function(
+  boolVariation: function (
     identifier: string,
     target: Target,
     defaultValue = false,
   ): boolean {
     return this.instance.boolVariation(identifier, target, defaultValue);
   },
-  stringVariation: function(
+  stringVariation: function (
     identifier: string,
     target: Target,
     defaultValue = '',
   ): string {
     return this.stringVariation(identifier, target, defaultValue);
   },
-  numberVariation: function(
+  numberVariation: function (
     identifier: string,
     target: Target,
     defaultValue = 0,
   ): number {
     return this.instance.numberVariation(identifier, target, defaultValue);
   },
-  jsonVariation: function(
+  jsonVariation: function (
     identifier: string,
     target: Target,
     defaultValue = '',
