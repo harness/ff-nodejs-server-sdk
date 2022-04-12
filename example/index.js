191a0fbca43deef767eb68b1aefcae06d4d82e80
Milos Vasic <milos.vasic@harness.io>
2022-04-12 12:46:40 +0300

    [FFM-3014] Example updated.

--- example/index.js
+++ example/index.js
@@ -9,8 +9,19 @@ setInterval(async () => {
   const target = {
     identifier: 'harness',
   };
-  const value = await client.boolVariation('test', target, false);
-  console.log('Evaluation for flag test and target: ', value, target);
+
+  const value = await client.boolVariation('flag1', target, false);
+  console.log('flag1: ', value, target);
+
+  const value2 = await client.numberVariation('flag2', target, -1);
+  console.log('flag2: ', value2, target);
+
+  const value3 = await client.stringVariation('flag3', target, "NO_VALUE!!!");
+  console.log('flag3: ', value3, target);
+
+  const value4 = await client.jsonVariation('flag4', target, {"ok": "NO_VALUE!"});
+  console.log('flag4: ', value4, target);
+
 }, 10000);
 
 console.log('Application started');
