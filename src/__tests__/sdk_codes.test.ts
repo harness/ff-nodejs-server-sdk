2b6ed69160e9c1e778b0c0c19ae17406e037947a
Erdi Rowlands <erdi.rowlands@harness.io>
2023-06-22 14:09:45 +0100

    FFM-8289 Test files

--- src/__tests__/sdk_codes.test.ts
+++ src/__tests__/sdk_codes.test.ts
@@ -1,7 +1,6 @@
 import { ConsoleLog, Logger } from "../log";
 import * as sdkCodes from '../sdk_codes';
 
-
 describe('SDK Tests', () => {
   let logger: Logger;
 
@@ -14,6 +13,27 @@ describe('SDK Tests', () => {
       sdkCodes.infoSDKInitOK(logger);
       sdkCodes.infoSDKCloseSuccess(logger);
       sdkCodes.infoMetricsThreadStarted(5000, logger);
+      sdkCodes.infoPollStarted(60, logger)
+      sdkCodes.infoSDKInitWaiting( logger)
+      sdkCodes.infoSDKStartClose( logger)
+      sdkCodes.infoSDKAuthOK( logger)
+      sdkCodes.infoPollingStopped("Dummy reason", logger)
+      sdkCodes.infoStreamConnected( logger)
+      sdkCodes.infoStreamEventReceived( "Dummy event", logger)
+      sdkCodes.infoStreamStopped( logger)
+      sdkCodes.infoMetricsSuccess( logger)
+      sdkCodes.infoMetricsTargetExceeded( logger)
+      sdkCodes.infoMetricsThreadExited( logger)
+      sdkCodes.infoEvalSuccess( logger)
+      sdkCodes.warnAuthFailedSrvDefaults( logger)
+      sdkCodes.warnMissingSDKKey( logger)
+      sdkCodes.warnFailedInitAuthError( logger)
+      sdkCodes.warnAuthFailedExceedRetries( logger)
+      sdkCodes.warnAuthRetrying(1, "dummy error", logger)
+      sdkCodes.warnStreamDisconnected("dummy reason", logger)
+      sdkCodes.warnStreamRetrying(4, logger)
+      sdkCodes.warnPostMetricsFailed("dummy error", logger)
+      sdkCodes.warnDefaultVariationServed("flag", "target", "default value", logger)
       // Call other functions here
     }).not.toThrow();
   });
